2b487ddbf2c794d81cd59deb4c300adee4066938
Phil McCloghry-Laing <phil@philistine.com.au>
2017-01-09 10:56:17 +1100

    Exercise 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from '@angular/material';
-import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { ContactsAppComponent } from './contacts.component';
+import { ContactsHeaderComponent } from './contacts-header';
 
 @NgModule({
-  declarations: [ContactsAppComponent],
-  imports: [
-    BrowserModule,
-    MaterialModule.forRoot(),
-    FlexLayoutModule.forRoot()
+  declarations: [
+    ContactsAppComponent,
+    ContactsHeaderComponent
   ],
+  imports: [BrowserModule],
   bootstrap: [ContactsAppComponent]
 })
 export class ContactsModule {
